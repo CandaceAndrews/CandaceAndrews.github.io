a8d3c189c03b2feb38cb6b5193994094f570e1d4
CandaceAndrews <candacewolfandrews@hotmail.com>
2023-12-05 20:18:36 -0500

    created AboutPage, ProjectsPage, ContactPage and Navbar components

---
+++ src/router/index.js
@@ -0,0 +1,6 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import AboutPage from '../components/AboutPage.vue';
+import ProjectsPage from '../components/ProjectsPage.vue';
+import ContactPage from '../components/ContactPage.vue';
+
