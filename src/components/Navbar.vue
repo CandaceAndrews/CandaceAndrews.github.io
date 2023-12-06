e76bf4a35a20cd2880367749f59ea0816a1614aa
CandaceAndrews <candacewolfandrews@hotmail.com>
2023-12-05 20:52:31 -0500

    navbar darker, white text and highlight when hovering over link

---
+++ src/components/Navbar.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="navbar">
+        <router-link to="/about" class="nav-link">About</router-link>
+        <router-link to="/projects" class="nav-link">Projects</router-link>
+        <router-link to="/contact" class="nav-link">Contact</router-link>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Navbar',
+}
+</script>
+
+<style>
+.navbar {
+  background-color: #333;
+  padding: 10px;
+  display: flex;
+  justify-content: space-around;
+}
+
+.nav-link {
+  color: #fff;
+  text-decoration: none;
+  padding: 5px 10px;
+  border-radius: 5px;
+  transition: background-color 0.3s, color 0.3s; /* Add color transition */
+
+  &:hover {
+    background-color: #555;
+    color: #fff; /* Change text color on hover */
+  }
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -4,3 +4,16 @@ import AboutPage from '../components/AboutPage.vue';
 import ProjectsPage from '../components/ProjectsPage.vue';
 import ContactPage from '../components/ContactPage.vue';
 
+Vue.use(VueRouter)
+
+const routes = [
+    { path: '/about', component: AboutPage },
+    { path: '/projects', component: ProjectsPage },
+    { path: '/contact', compontent: ContactPage },
+]
+
+const router = new VueRouter({
+    routes,
+})
+
+export default router
