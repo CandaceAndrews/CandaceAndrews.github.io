ef84bd9f2a7734c7ff5673b3f2a6737075e36372
CandaceAndrews <candacewolfandrews@hotmail.com>
2023-12-28 18:10:34 -0500

    added styling CSS to ContactPage form

--- src/components/ContactPage.vue
+++ src/components/ContactPage.vue
@@ -2,14 +2,20 @@
     <div class="contact-page">
       <h1>Contact Page</h1>
       <form @submit.prevent="submitForm">
-        <label for="name">Name:</label>
-        <input type="text" id="name" v-model="formData.name" required>
+        <div class="form-group">
+          <label for="name">Name:</label>
+          <input type="text" id="name" v-model="formData.name" required>
+        </div>
 
-        <label for="email">Email:</label>
-        <input type="email" id="email" v-model="formData.email" required>
+        <div class="form-group">
+          <label for="email">Email:</label>
+          <input type="email" id="email" v-model="formData.email" required>
+        </div>
 
-        <label for="message">Message:</label>
-        <textarea id="message" v-model="formData.message" required></textarea>
+        <div class="form-group">
+          <label for="message">Message:</label>
+          <textarea id="message" v-model="formData.message" required></textarea>
+        </div>
 
         <button type="submit">Submit</button>
       </form>
@@ -42,8 +48,49 @@
   </script>
   
   <style scoped>
- h1 {
+.contact-page {
+  max-width: 600px;
+  margin: 0 auto;
+  padding: 20px;
+}
+
+.contact-form {
+  display: flex;
+  flex-direction: column;
+}
+
+.form-group {
+  margin-bottom: 20px;
+}
+
+h1 {
     margin-top: 52px;
 }
+
+label {
+  font-weight: bold;
+  margin-bottom: 8px;
+}
+
+input,
+textarea {
+  width: 100%;
+  padding: 8px;
+  box-sizing: border-box;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  margin-top: 4px;
+}
+
+button {
+  background-color: #007BFF;
+  color: #fff;
+  padding: 10px 20px;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+  font-size: 16px;
+}
+
   </style>
   
